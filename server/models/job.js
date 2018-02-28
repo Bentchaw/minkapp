80d43ada5a6feb134017012c1868ada733273325
bentchaw <tanrithy.rt@gmail.com>
2018-02-28 14:12:10 +0100

    Job model

--- server/models/job.js
+++ server/models/job.js
@@ -2,8 +2,26 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const jobSchema = new Schema({
-  name: String,
-  adress: String
+  title: String,
+  description: String,
+  cityName: String,
+  contract: String,
+  contractDuration: Number,
+  weeklyWorkTime: String,
+  weeklyWorkTimeTypeName: String,
+  minSalary: Number,
+  experienceName: String,
+  skills: [
+    {
+      type: String
+    }
+  ],
+  professionalQualities: [
+    {
+      type: String
+    }
+  ],
+  activityName: String
 });
 
 module.exports = mongoose.model("Job", jobSchema);
