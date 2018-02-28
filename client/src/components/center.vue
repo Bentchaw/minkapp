dfb1523d91068ff975d45222e28bb8bdb9ce0dde
Jeff Campigli <jfcampigli@gmail.com>
2018-02-28 18:08:36 +0100

    add login register

--- client/src/components/center.vue
+++ client/src/components/center.vue
@@ -1,36 +1,44 @@
 <template>
-
+<section>
 <div class="columns" >
+
+ 
   <div class="column">
 
 
-  <h1>Solidarité</h1>
+  <h1>Devenez coach</h1>
 <div class="contenu">
-  <h2>La solidarité est notre moteur</h2>
-  <br>
-Mink offre une mise en relation par competences recherchées ou offertes, garantissant ansi la pertinence des "matchs"
-
-
-
-
-
+  <img src="/images/coach2.png" alt="mains"width="120px" >
+  <h2>Aidez des demandeurs d'emploi en leur offrant vos talents</h2>
+  </div>
 </div>
 
 
 
-  </div>
   <div class="column">
-    Pourquoi
+     <h1>Candidats</h1><br><br>
+<div class="contenu">
+  <img src="/images/candidat.png" alt="mains"width="155px" >
+  <h2>grâce à un accompagnement soigné développez vos competénces, retrouvez une dynamique positive, et surtout trouvez un job!  Activez la Solidarité
+</h2>
   </div>
+    </div>
+   
+
+
   <div class="column">
-   Comment
+
+
+ <h1>Entreprise</h1><br><br><br>
+<div class="contenu">
+  <img src="/images/entreprise.png" alt="mains"width="180px" >
+  <h2>Déposez vos offres d'emploi et dénichez des candidats motivés et éfficaces</h2>
+  </div>
   </div>
   
-</div>
-
-
 
 </div>
+</section>
 
 
 
